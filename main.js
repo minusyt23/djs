97b99e596d79fb655bd8d461e3dcae9bfcb5516e
minusyt23 <altirtertyo@gmail.com>
2020-11-06 13:26:59 +0100

    First

---
+++ main.js
@@ -0,0 +1,28 @@
+const Electron = require("electron");
+const Speaker = require("speaker");
+
+function createWindow () {
+  const win = new Electron.BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile("index.html");
+}
+
+Electron.app.whenReady().then(createWindow)
+
+Electron.app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    Electron.app.quit();
+  }
+});
+
+Electron.app.on("activate", () => {
+  if (Electron.BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
